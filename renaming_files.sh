e534ae859f311fb31da4613e5d7f577355c05298
mrvms <mrsho1@outlook.com>
2023-02-15 12:16:00 -0600

    Initial commit

---
+++ renaming_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Author : OS
+#Date   : Feb 15 2023
+#Des    : Renaming files with date and time
+
+
+# Get current date and time
+  DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Loop through each argument and rename the file
+  for file in "$@"
+  do
+  # Check if the file exists
+  if [ -f "$file" ]
+  then
+    # Rename the file with the current date and time
+    mv "$file" "${file}_${DATE}"
+    else
+    echo "File $file does not exist."
+  fi
+done
